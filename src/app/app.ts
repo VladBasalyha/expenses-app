8d9dded7bc42104dd91a32b24ccb2ac91fb08649
Vlad <basalyga.180999@gmail.com>
2025-11-29 19:10:04 +0200

    list markup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ExpenseList } from "./expense-list/expense-list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ExpenseList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-app');
+  protected readonly title = signal('Expenses list');
 }
