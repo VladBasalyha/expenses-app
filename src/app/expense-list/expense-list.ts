ae8568cd7874c64bb08bfd25a7a4be08b9e7ad57
Vlad <basalyga.180999@gmail.com>
2025-12-09 22:12:24 +0200

    added button

--- src/app/expense-list/expense-list.ts
+++ src/app/expense-list/expense-list.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import expenses from '../mock/expenses.json'
+import expenses from '../mock/expenses.json';
 
 @Component({
   selector: 'app-expense-list',
-
   templateUrl: './expense-list.html',
   styleUrl: './expense-list.css',
 })
 export class ExpenseList {
-  title = "app-expense-list";
-  expenses = expenses
+  title = 'app-expense-list';
+  expenses = expenses;
 }
